fd220a0caceabc42a37a7dea0839f48b11152b27
MatKac <MateuszKacala@users.noreply.github.com>
2019-03-06 00:25:40 +0100

    Faster data conversion

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -3,13 +3,15 @@ using NetMQ.Sockets;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 
 public class Client : MonoBehaviour
 {
 
     private static SubscriberSocket client;
-    private Vector2 data;
+    private static Vector2 data;
     public Vector2 Data { get => data; }
 
 
@@ -37,14 +39,35 @@ public class Client : MonoBehaviour
 
     private static void ReadData()
     {
-        byte[] bytes;
-        if (client.TryReceiveFrameBytes(out bytes))
+        string reply = client.ReceiveFrameString();
+        //if (client.TryReceiveFrameString(out reply))
         {
-            //TODO: konwersja z bytes do Vectora liczb
-            Debug.Log("");
-            //Console.WriteLine("> " + reply);
+            string[] s = reply.Split(' ');
+            s = s.Where(val => System.Text.RegularExpressions.Regex.IsMatch(val, @"\d")).ToArray();
+            data.x = Convert.ToInt32(s[0]);
+            data.y = Convert.ToInt32(s[1]);
         }
     }
+
+    private static void ReadData2()
+    {
+        byte[] reply;
+        if (client.TryReceiveFrameBytes(out reply))
+        {
+            for(int i =0; i < reply.Length;i++)
+                if (reply[i] > 0x39 || reply[i] < 0x30) reply[i] = 0x30;
+            int x = 0;
+            for (int i = 0; i <= 2; i++)
+                x = 10 * x + reply[i] - 48;
+            data.x = x;
+            x = 0;
+            for (int i = 4; i <= 6; i++)
+                x = 10 * x + reply[i] - 48;
+            data.y = x;
+            //Debug.Log(Encoding.UTF8.GetString(x1, 0, x1.Length) + "<->" + Encoding.UTF8.GetString(x2, 0, x2.Length));
+        }
+    }
+
 
     void Start()
     {
@@ -55,9 +78,10 @@ public class Client : MonoBehaviour
 
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        ReadData();
+        ReadData2();
+        Debug.Log(data.x.ToString() + " " + data.y.ToString());
     }
 
     private void OnDestroy()
