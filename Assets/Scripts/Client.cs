645744ef6867a0f021a3ff7ddd92b6b276b1c0ed
MatKac <MateuszKacala@users.noreply.github.com>
2019-03-07 19:40:26 +0100

    Update Client.cs

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -37,51 +37,44 @@ public class Client : MonoBehaviour
         Debug.Log("Server connected!");
     }
 
+    //download data as long as possible
+    private static void ReadData()
+    {
+        string reply;
+        while(client.TryReceiveFrameString(out reply))
+        {
+            string[] s = reply.Split(' ');
+            int i = 0;
+            while (s[i].Length < 1) i++;
+            data.x = Convert.ToInt32(s[i]); i++;
+            while (s[i].Length < 1) i++;
+            data.y = Convert.ToInt32(s[i]);
+        }
+    }
+    /*
+    //download data once
     private static void ReadData()
     {
         string reply = client.ReceiveFrameString();
-        //if (client.TryReceiveFrameString(out reply))
-        {
-            string[] s = reply.Split(' ');
-            s = s.Where(val => System.Text.RegularExpressions.Regex.IsMatch(val, @"\d")).ToArray();
-            data.x = Convert.ToInt32(s[0]);
-            data.y = Convert.ToInt32(s[1]);
-        }
+        string[] s = reply.Split(' ');
+        int i = 0;
+        while (s[i].Length < 1) i++;
+        data.x = Convert.ToInt32(s[i]); i++;
+        while (s[i].Length < 1) i++;
+        data.y = Convert.ToInt32(s[i]);
     }
-
-    private static void ReadData2()
-    {
-        byte[] reply;
-        if (client.TryReceiveFrameBytes(out reply))
-        {
-            for(int i =0; i < reply.Length;i++)
-                if (reply[i] > 0x39 || reply[i] < 0x30) reply[i] = 0x30;
-            int x = 0;
-            for (int i = 0; i <= 2; i++)
-                x = 10 * x + reply[i] - 48;
-            data.x = x;
-            x = 0;
-            for (int i = 4; i <= 6; i++)
-                x = 10 * x + reply[i] - 48;
-            data.y = x;
-            //Debug.Log(Encoding.UTF8.GetString(x1, 0, x1.Length) + "<->" + Encoding.UTF8.GetString(x2, 0, x2.Length));
-        }
-    }
-
+    */
 
     void Start()
     {
         StartClient();
     }
 
-    
-
-
     // Update is called once per frame
     void Update()
     {
-        ReadData2();
-        Debug.Log(data.x.ToString() + " " + data.y.ToString());
+        ReadData();
+        //Debug.Log(data.x.ToString() + " " + data.y.ToString());
     }
 
     private void OnDestroy()
