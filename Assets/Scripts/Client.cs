c3198677d8c05e3ddff0e6ac0601a8c3f3886909
renonpl <domi.hawr@gmail.com>
2019-03-18 00:24:06 +0100

    OSX Test Panel

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -1,18 +1,53 @@
-﻿using NetMQ;
+﻿#if UNITY_EDITOR_WIN
+using NetMQ;
 using NetMQ.Sockets;
+#endif
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Client : MonoBehaviour
 {
-
-    private static SubscriberSocket client;
     private static Vector2 data;
     public Vector2 Data { get => data; }
+    public Canvas can;
+    public GameObject panel;
+
+#if UNITY_EDITOR_OSX
+    private RectTransform rtrans;
+    private Vector3 offset;
+
+    void Start()
+    {
+        panel.gameObject.SetActive(true);
+        rtrans = panel.GetComponent<RectTransform>();
+        offset = panel.transform.position;
+        data = new Vector2(0, 0);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) ReadInput();
+    }
+
+    void ReadInput()
+    {
+        Vector2 mousePos = Input.mousePosition - offset;
+        float currentX = mousePos.x * 200 / rtrans.rect.width; //200, because server operates with ints in range [-100,100]
+        float currentY = mousePos.y * 200 / rtrans.rect.height;
+        if (currentX >= -100 && currentX <= 100 && currentY >= -100 && currentY <= 100)
+        {
+            data = new Vector2((int)currentX, (int)currentY);
+        }
+        //Debug.Log(data.ToString());
+    }
+#endif
+#if UNITY_EDITOR_WIN
+    private static SubscriberSocket client;
 
 
     /*
@@ -25,7 +60,7 @@ public class Client : MonoBehaviour
         try
         {
             client = new SubscriberSocket();
-            string connect_to = "tcp://"+ server_address +":" + port.ToString();
+            string connect_to = "tcp://" + server_address + ":" + port.ToString();
             client.Connect(connect_to);
             client.Subscribe("");
         }
@@ -37,11 +72,11 @@ public class Client : MonoBehaviour
         Debug.Log("Server connected!");
     }
 
-    //download data as long as possible
+    /*download data as long as possible*/
     private static void ReadData()
     {
         string reply;
-        while(client.TryReceiveFrameString(out reply))
+        while (client.TryReceiveFrameString(out reply))
         {
             string[] s = reply.Split(' ');
             int i = 0;
@@ -51,22 +86,10 @@ public class Client : MonoBehaviour
             data.y = Convert.ToInt32(s[i]);
         }
     }
-    /*
-    //download data once
-    private static void ReadData()
-    {
-        string reply = client.ReceiveFrameString();
-        string[] s = reply.Split(' ');
-        int i = 0;
-        while (s[i].Length < 1) i++;
-        data.x = Convert.ToInt32(s[i]); i++;
-        while (s[i].Length < 1) i++;
-        data.y = Convert.ToInt32(s[i]);
-    }
-    */
 
     void Start()
     {
+        can.gameObject.SetActive(false);
         StartClient();
     }
 
@@ -74,7 +97,7 @@ public class Client : MonoBehaviour
     void Update()
     {
         ReadData();
-        //Debug.Log(data.x.ToString() + " " + data.y.ToString());
+      //  Debug.Log(data.x.ToString() + " " + data.y.ToString());
     }
 
     private void OnDestroy()
@@ -82,4 +105,5 @@ public class Client : MonoBehaviour
         client.Dispose();
         NetMQConfig.Cleanup();
     }
+#endif
 }
